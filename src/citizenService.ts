440e2f571b7815cc7b2f537374ec8bcd9eec2667
Grigori Fiodorov <grigori.fiodorov@gmail.com>
2023-10-29 01:13:59 +0300

    if the president already exists with the same tg id then show error about bad req added http-error lib to throw error from above

--- src/citizenService.ts
+++ src/citizenService.ts
@@ -1,12 +1,19 @@
 import {InputAdminInit, OutputAdminInit} from "./types";
 import {citizenDbController} from "./dbController";
 import {toDataURL} from "qrcode";
+import createHttpError from "http-errors";
 
 export const presidentInit = async (inputAdminInit: InputAdminInit): Promise<OutputAdminInit> => {
     const blockId = Math.random().toString(36).substring(7)
 
     const citizen = await citizenDbController
         .presidentInit(blockId, inputAdminInit.telegramUserId)
+
+    const isPresidentExists = await citizenDbController.isPresidentExists(inputAdminInit.telegramUserId)
+
+    if (isPresidentExists) {
+        throw createHttpError(400, "President already exists")
+    }
 
     const url = `https://t.me/${process.env.TELEGRAM_BOT_USERNAME}?init=${citizen.blockId}`
 
--- src/dbController.ts
+++ src/dbController.ts
@@ -3,6 +3,16 @@ import {Citizen, CitizenTypes} from "./types";
 
 class CitizenDbController {
 
+    isPresidentExists = async (telegramUserId: string): Promise<boolean> => {
+        const president = await citizenMongooseModel.exists({
+            type: CitizenTypes.President,
+            telegramUserId
+        }).exec()
+
+        return president !== null
+
+    }
+
     presidentInit = async (blockId: string, telegramUserId: string): Promise<Citizen> => {
 
         const newCitizen = new citizenMongooseModel({
